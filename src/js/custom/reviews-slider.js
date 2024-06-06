ecf00f3400f74dc8ecfa123246b10e72583d0496
Slava Sidorov <sidorov-vv3@mail.ru>
2024-06-06 15:58:54 +0700

    first commit

---
+++ src/js/custom/reviews-slider.js
@@ -0,0 +1,44 @@
+// Required Core Stylesheet
+import "@glidejs/glide/dist/css/glide.core.min.css";
+
+// Optional Theme Stylesheet
+import "@glidejs/glide/dist/css/glide.theme.min.css";
+
+import Glide from '@glidejs/glide';
+
+var glide = new Glide(".glide", {
+  type: "carousel",
+  focusAt: "center",
+  perView: 3,
+  breakpoints: {
+    1200: {
+      perView: 3
+    },
+    1024: {
+      perView: 2
+    },
+    768: {
+      perView: 1
+    }
+  }
+});
+glide.mount();
+
+
+var glide = new Glide(".glide-publications", {
+  type: "carousel",
+  focusAt: "center",
+  perView: 3,
+  breakpoints: {
+    1200: {
+      perView: 3
+    },
+    1024: {
+      perView: 2
+    },
+    768: {
+      perView: 1
+    }
+  }
+});
+glide.mount();
